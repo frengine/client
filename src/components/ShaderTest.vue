823ab660d7e82f24d718fd4363c5f540aa7d7ba6
timostrating <timostrating@home.nl>
2019-05-08 18:50:15 +0200

    we have shaders now

--- src/components/ShaderTest.vue
+++ src/components/ShaderTest.vue
@@ -7,12 +7,13 @@
       </v-flex>
       <v-flex xs12 sm6 mb-5>
         <h2>Fragment shader</h2>
-        <v-btn color="info" @click="reRender()">random color</v-btn>
-        <v-text-field
-				   type="text" 
-				   placeholder="TODO" 
-				   @keypress="reRender()"
-				   ></v-text-field>
+        <v-btn color="info" @click="randomColor()">random color</v-btn>
+        <v-btn color="info" @click="shader()">run shader</v-btn>
+          <textarea id="text-inp" width="" height="200">
+void main(void) {
+  gl_FragColor = vec4(gl_FragCoord.x / 640.0, gl_FragCoord.y / 480.0, 0, 1);
+}
+          </textarea>
       </v-flex>
     </v-layout>
   </v-container>
@@ -39,14 +40,65 @@
         this.gl.clear(this.gl.COLOR_BUFFER_BIT);
       },
 
-      reRender() {
+      randomColor() {
         this.gl.clearColor(Math.random(1), Math.random(1), Math.random(1), 1);
         this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+      },
+
+      shader() {
+        console.log("Its shader time");
+        // Vertex Shader
+        var vertCode =
+          'attribute vec2 a_position;' +
+          
+          'void main(void) {' +
+              ' gl_Position = vec4(a_position, 0, 1);' +
+          '}';
+
+        var vertexShader = this.gl.createShader(this.gl.VERTEX_SHADER);
+        this.gl.shaderSource(vertexShader, vertCode);
+        this.gl.compileShader(vertexShader);
+        
+        // Fragment Shader
+        var fragCode = String(document.getElementById("text-inp").value);
+
+        var fragmentShader = this.gl.createShader(this.gl.FRAGMENT_SHADER);
+        this.gl.shaderSource(fragmentShader, fragCode);
+        this.gl.compileShader(fragmentShader);
+
+
+        var program = this.gl.createProgram();
+        this.gl.attachShader(program, vertexShader);
+        this.gl.attachShader(program, fragmentShader);
+        this.gl.linkProgram(program);	
+        this.gl.useProgram(program);
+
+
+
+        var buffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer);
+        this.gl.bufferData(
+          this.gl.ARRAY_BUFFER, 
+          new Float32Array([
+            -1.0, -1.0, 
+            1.0, -1.0, 
+            -1.0,  1.0, 
+            -1.0,  1.0, 
+            1.0, -1.0, 
+            1.0,  1.0]), 
+          this.gl.STATIC_DRAW
+        );
+
+        var positionLocation = this.gl.getAttribLocation(program, "a_position");
+        this.gl.enableVertexAttribArray(positionLocation);
+        this.gl.vertexAttribPointer(positionLocation, 2, this.gl.FLOAT, false, 0, 0);
+
+        this.gl.drawArrays(this.gl.TRIANGLES, 0, 6);
       }
     }
   }
 </script>
 
 <style>
-
+  #text-inp { z-index:999; width: 300px; height: 300px;   }
 </style>
